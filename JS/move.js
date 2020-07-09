e453e4683b5eb0bc93ae665e010db9850ab4ff4a
akira <akiratsuraii@gmail.com>
2020-07-09 17:34:40 +0800

    v1

---
+++ JS/move.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $('#toAbout').click(function()
+	{
+  	    $('html,body').animate({ scrollTop:$('#about').offset().top}, 900);
+	}); 
+});
+
+$(document).ready(function(){
+  $('#toSkills').click(function()
+	{
+  	    $('html,body').animate({ scrollTop:$('#skills').offset().top}, 900);
+	}); 
+});
+
+$(document).ready(function(){
+  $('#toProject').click(function()
+	{
+  	    $('html,body').animate({ scrollTop:$('#project').offset().top}, 900);
+	}); 
+});
+
